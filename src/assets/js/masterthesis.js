0c3bde13d0082d0f673fb3d342fc3118bc905cf6
Sophia Mersmann <sophia.mersmann1@gmail.com>
2020-06-13 09:56:15 +0200

    [UPDATE] Add table of content

--- src/assets/js/masterthesis.js
+++ src/assets/js/masterthesis.js
@@ -1,5 +1,7 @@
 import generateReferences from './modules/references.js';
 
+// TODO: Import colors from scss
+
 function updateProgressBar(selector, docHeight, winHeight) {
   // determine scroll position in current window
   const scrollTop = $(window).scrollTop();
@@ -14,6 +16,34 @@ function scrollToTarget(event) {
   if (target) {
     $('html, body').animate({ scrollTop: target.offset().top - 15 }, 500);
   }
+}
+
+function animateTableOfContent() {
+  // after a delay, hide the table of content
+  const tocWrapper = $('.table-of-content-wrapper');
+  const toc = $('#table-of-content');
+  setTimeout(() => {
+    tocWrapper.animate({ left: `${-toc.width()}px` }, 'slow');
+    setTimeout(() => toc.css('border-right', '3px solid #fcba03'), 600);
+  }, 3000);
+
+  // on mouse enter, show the table of content
+  tocWrapper.mouseenter(() => {
+    // if elem is currently being animated, don't do anything
+    if (tocWrapper.is(':animated')) return;
+
+    toc.css('border-right', 'none');
+    tocWrapper.animate({ left: 0 }, 'slow');
+  });
+
+  // one mouse leave, hide the table of content
+  tocWrapper.mouseleave(() => {
+  // if elem is currently being animated, don't do anything
+    if (tocWrapper.is(':animated')) return;
+
+    tocWrapper.animate({ left: `${-toc.width()}px` }, 'slow');
+    setTimeout(() => toc.css('border-right', '3px solid #fcba03'), 600);
+  });
 }
 
 $(document).ready(() => {
@@ -37,4 +67,7 @@ $(document).ready(() => {
 
   // on click, scroll to target
   $('a[href^="#"]').click(scrollToTarget);
+
+  // animate table of content
+  animateTableOfContent();
 });
