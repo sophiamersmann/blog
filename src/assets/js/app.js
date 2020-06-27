7f2a6d2cc28218b8d803ba5009de3f1f9fbe876b
Sophia Mersmann <sophia.mersmann1@gmail.com>
2020-06-27 11:50:41 +0200

    Remove master thesis code
    The master thesis has been moved to a different repo.

--- src/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-window.$ = window.jquery = require('jquery');
-window.math = require('mathjs');
-require('bootstrap');
-
-require('./thesis.js');
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: 'production',
-  entry: './src/assets/js/app.js',
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'bundle.min.js',
-  }
-};
